33e3871e7900974a0c3a8117516b05364400da95
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 08:49:21 -0400

    Add files via upload

---
+++ Sign-Cert.ps1
@@ -0,0 +1,5 @@
+﻿#Get Cert From Cert Store, Requires Cert Already Be Imported
+$cert = (dir Cert:\CurrentUser\TrustedPublisher -CodeSigningCert) 
+#Asks user where scripts to sign are located
+$ScriptToSign = (Get-ChildItem -Path (Read-Host -Prompt 'Get path') -Filter '*.ps1' | Select Fullname).Fullname
+Set-AuthenticodeSignature $ScriptToSign -Certificate $Cert
